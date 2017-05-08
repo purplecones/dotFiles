0967e9ad0742625a2512ae8c4fa6e7a924500703
Mirza Joldic <mirza.joldic@gmail.com>
2017-05-08 16:52:43 -0400

    added hyper.js

--- symlinks.sh
+++ symlinks.sh
@@ -3,3 +3,4 @@ ln -s ~/.dotfiles/zsh/zshrc-custom ~/.zshrc-custom
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim/vim-plugins ~/.vim-plugins
+ln -s ~/.dotfiles/hyper/hyper.js ~/.hyper.js
