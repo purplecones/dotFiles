0fa6523caf91064a042a7d5e3b9341c9711c9c85
Mirza Joldic <mirza.joldic@gmail.com>
2017-05-08 16:48:32 -0400

    initial dotgiles setup

---
+++ symlinks.sh
@@ -0,0 +1,5 @@
+# set symlinks
+ln -s ~/.dotfiles/zsh/zshrc-custom ~/.zshrc-custom
+ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/vim-plugins ~/.vim-plugins
