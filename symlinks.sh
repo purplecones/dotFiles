4f76991f237fe049cb9895fbb6b50ce4f0fadfbf
Mirza Joldic <mirza.joldic@gmail.com>
2017-10-26 09:09:55 -0400

    .

--- symlinks.sh
+++ symlinks.sh
@@ -3,7 +3,6 @@ ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh 
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim/vim-plugins ~/.vim-plugins
 ln -s ~/.dotfiles/hyper/hyper.js ~/.hyper.js
 ln -s ~/.dotfiles/atom ~/.atom
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
