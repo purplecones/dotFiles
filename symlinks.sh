cb7b0f48ecde8ff4d6de62ff781aa5f0ad2bb481
Mirza Joldic <mirza.joldic@gmail.com>
2019-11-22 14:37:35 -0500

    resync dotfile:w

--- symlinks.sh
+++ symlinks.sh
@@ -1,9 +1,10 @@
-ln -s ~/.dotfiles/zsh/zshrc-custom ~/.zshrc-custom
-ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/.dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh 
-ln -s ~/.dotfiles/vim ~/.vim
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/.dotfiles/hyper/hyper.js ~/.hyper.js
-ln -s ~/.dotfiles/atom ~/.atom
-ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/vscode/User ~/Library/Application\ Support/Code/User
+ln -s ~/dotfiles/zsh/zshrc-custom ~/.zshrc-custom
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh ~/.zsh
+ln -s ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh 
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/hyper/hyper.js ~/.hyper.js
+ln -s ~/dotfiles/atom ~/.atom
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vscode/User ~/Library/Application\ Support/Code/User
